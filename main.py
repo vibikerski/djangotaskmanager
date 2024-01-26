79f30aecadc463ed3cae8dd3f9ef7d2397642660
vibikerski <vibikerski@gmail.com>
2024-01-26 18:58:25 +0100

    Added tasks, requirements

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import django_setup
 from tasks.models import Status, User, Task
+
+in_process = Status.objects.get(title="In process")
+completed = Status.objects.get(title="Completed")
+postponed = Status.objects.get(title="Postponed")
+
+john_doe = User.objects.get(name="John Doe", email="johndoe@johndoe.johndoe")
+jane_doe = User.objects.get(name="Jane Doe", email="janedoe@email.com")
+
+task1 = Task.objects.get(id=1)
+task2 = Task.objects.get(id=2)
+task3 = Task.objects.get(id=3)
+
+task3.creator = jane_doe
+task3.save()
